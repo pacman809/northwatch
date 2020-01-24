42e8b57f1738aaac75272e69e1681d07e64f477b
pacman809 <garthmadden809@gmail.com>
2020-01-23 23:35:16 -0600

    Added comments to Search Query

--- HaloWebsite/searchQuery.py
+++ HaloWebsite/searchQuery.py
@@ -1,19 +1,21 @@
 def Query(search):
 
 	search = str(search)
-
 	error = None
 
 	if len(search) == 42:
-		search = "https://www.haloexplorer.com/balance/{}".format(search)
+		search = "https://www.haloexplorer.com/balance/{}".format(search) # is this an address?
 		return search
+
 	if len(search) == 66:
-		search = "https://www.haloexplorer.com/tx/{}".format(search)
+		search = "https://www.haloexplorer.com/tx/{}".format(search)#is this a transaction?
 		return search
+
 	if len(search) <= 8:
 		try:
 		   val = int(search)
-		   search = "https://www.haloexplorer.com/block/{}".format(search)
+		   search = "https://www.haloexplorer.com/block/{}".format(search)# is this a block number / invalid at block 100000000 Y2K
 		   return search
+
 		except ValueError:
 		   return error
