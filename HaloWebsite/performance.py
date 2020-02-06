4e04cf5afbc2f971638fd89ea8eb852239b800b1
pacman809 <garthmadden809@gmail.com>
2020-02-05 21:10:38 -0600

    Cleaned and synced Performance.py

--- HaloWebsite/performance.py
+++ HaloWebsite/performance.py
@@ -1,9 +1,5 @@
-
-import pymongo
 import datetime
-from web3 import Web3
-from search import connect_geth
-
+from data import database, connect_geth
 
 
     
@@ -13,32 +9,22 @@ from search import connect_geth
 def performance():
 
 		
-		db 			= "halo-explorer-mainnet"
-		myclient 	= pymongo.MongoClient("mongodb://localhost:27017/")
-		mydb 		= myclient[db]
+		mydb 		= database()
 		myCol 		= mydb["blocks"]
-		geth_url 	= "http://192.168.1.139:8545"
-		list 		= []
-		MN_Array 	= []
 
 		
-		#print(times)
 		web3 = connect_geth()
 		blockNumber2 = web3.eth.blockNumber  #BLOCKNUMBER RESULT
 		blockNumber = blockNumber2
 		previousBlockNumber = blockNumber - 5
 		blockNumber = previousBlockNumber + 1
 		averageBlock = blockNumber2 - 1000000
-		#print(previousBlockNumber)
 
 		for x in myCol.find({"number": int(blockNumber)}):
 			blockNumber = x["timestamp"]
-			#print(blockNumber)
-
 
 			for y in myCol.find({"number": previousBlockNumber}):
 				previousBlockNumber = y["timestamp"]
-				#print(previousBlockNumber)
 
 				for z in myCol.find({"number": averageBlock}):
 					averageBlock = z["timestamp"]
@@ -47,16 +33,13 @@ def performance():
 
 		average = blockNumber - averageBlock
 		average = average / 1000000
-		average = average / 100000000000
+		average = average / 1000000000
 		blockTime = blockNumber - previousBlockNumber
 		blockTime = blockTime / 1000000000
 		times =  datetime.datetime.utcnow() 
 
 		#ROUNDING
 
-		
-
-		#print(blockTime)
 		performance={
 			"BLOCKNUMBER":	blockNumber2,
 			"TIME":			times,
