404842086a67ee4337c3c6cbedcfd92409eb6303
pacman809 <garthmadden809@gmail.com>
2020-02-05 21:13:49 -0600

    Performance.py now a function

--- HaloWebsite/data.py
+++ HaloWebsite/data.py
@@ -1,5 +1,5 @@
 import pymongo
-from datetime import datetime
+import datetime
 from web3 import Web3
 #--------------------CONFIG--------------------------------#
 
@@ -36,3 +36,46 @@ def connect_geth():
 	web3 = Web3(Web3.HTTPProvider(GethUrl))
 	
 	return web3
+
+def performance():
+
+		
+		mydb 		= database()
+		myCol 		= mydb["blocks"]
+
+		
+		web3 = connect_geth()
+		blockNumber2 = web3.eth.blockNumber  #BLOCKNUMBER RESULT
+		blockNumber = blockNumber2
+		previousBlockNumber = blockNumber - 5
+		blockNumber = previousBlockNumber + 1
+		averageBlock = blockNumber2 - 1000000
+
+		for x in myCol.find({"number": int(blockNumber)}):
+			blockNumber = x["timestamp"]
+
+			for y in myCol.find({"number": previousBlockNumber}):
+				previousBlockNumber = y["timestamp"]
+
+				for z in myCol.find({"number": averageBlock}):
+					averageBlock = z["timestamp"]
+		
+
+
+		average = blockNumber - averageBlock
+		average = average / 1000000
+		average = average / 1000000000
+		blockTime = blockNumber - previousBlockNumber
+		blockTime = blockTime / 1000000000
+		times =  datetime.datetime.utcnow() 
+
+		#ROUNDING
+
+		performance={
+			"BLOCKNUMBER":	blockNumber2,
+			"TIME":			times,
+			"BLOCKTIME":	blockTime,
+			"AVERAGE":		average
+		}
+
+		return performance
