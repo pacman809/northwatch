d852c3241465430faff2b14c14d063d6061c1ff6
pacman809 <garthmadden809@gmail.com>
2020-02-05 21:18:58 -0600

    Added files

---
+++ HaloWebsite/FlaskApp/static/img/COINS/hangman.py
@@ -0,0 +1,108 @@
+
+
+import requests
+import random
+import time
+
+import os
+#from prompt_toolkit import Application
+print "\x1b[8;50;1000t"
+os.system('clear')  # For Linux/OS X
+
+
+print(''' .----------------.  .----------------.  .-----------------. .----------------.  .----------------.  .----------------.  .-----------------.
+| .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |
+| |  ____  ____  | || |      __      | || | ____  _____  | || |    ______    | || | ____    ____ | || |      __      | || | ____  _____  | |
+| | |_   ||   _| | || |     /  \     | || ||_   \|_   _| | || |  .' ___  |   | || ||_   \  /   _|| || |     /  \     | || ||_   \|_   _| | |
+| |   | |__| |   | || |    / /\ \    | || |  |   \ | |   | || | / .'   \_|   | || |  |   \/   |  | || |    / /\ \    | || |  |   \ | |   | |
+| |   |  __  |   | || |   / ____ \   | || |  | |\ \| |   | || | | |    ____  | || |  | |\  /| |  | || |   / ____ \   | || |  | |\ \| |   | |
+| |  _| |  | |_  | || | _/ /    \ \_ | || | _| |_\   |_  | || | \ `.___]  _| | || | _| |_\/_| |_ | || | _/ /    \ \_ | || | _| |_\   |_  | |
+| | |____||____| | || ||____|  |____|| || ||_____|\____| | || |  `._____.'   | || ||_____||_____|| || ||____|  |____|| || ||_____|\____| | |
+| |              | || |              | || |              | || |              | || |              | || |              | || |              | |
+| '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |
+ '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------' ''')
+print ".......... Loading Word From Internet!!! Ooooohhhh"
+
+word_site = "http://svnweb.freebsd.org/csrg/share/dict/words?view=co&content-type=text/plain"
+response = requests.get(word_site)
+WORDS = response.content.splitlines()
+
+maxLength = len(WORDS)
+word = random.randint(0, maxLength)
+
+print ""
+
+time.sleep(1)
+
+print "Take a Guess Dumbo ..."
+time.sleep(0.5)
+
+word = WORDS[word]
+
+guesses = ''
+
+turns = 10
+
+while turns > 0:         
+
+    # make a counter that starts with zero
+    failed = 0             
+
+    # for every character in secret_word    
+    for char in word:      
+
+    # see if the character is in the players guess
+        if char in guesses:    
+    
+        # print then out the character
+            print char,    
+
+        else:
+    
+        # if not found, print a dash
+            print "_",     
+       
+        # and increase the failed counter with one
+            failed += 1    
+
+    # if failed is equal to zero
+
+    # print You Won
+    if failed == 0:        
+        print "You won"  
+
+    # exit the script
+        break              
+
+    print
+
+    # ask the user go guess a character
+    guess = raw_input("guess a character:") 
+
+    # set the players guess to guesses
+    guesses += guess                    
+
+    # if the guess is not found in the secret word
+    if guess not in word:  
+ 
+     # turns counter decreases with 1 (now 9)
+        turns -= 1        
+ 
+    # print wrong
+        print "Wrong"    
+ 
+    # how many turns are left
+        print "You have", + turns, 'more guesses' 
+ 
+    # if the turns are equal to zero
+        if turns == 0:           
+    
+        # print "You Lose"
+            print "You Lose" 
+            print ""
+            print "The Word Was..."
+            time.sleep(.5)
+            print "...."
+            time.sleep(.25)
+            print ""
+            print(word)
